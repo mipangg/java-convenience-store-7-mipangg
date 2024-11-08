58d28c1048cd29f5447cbb59a51787f6d096f08f
mipangg <morganjeong119@gmail.com>
2024-11-08 11:54:26 +0900

    feat(PromotionsFileReader): load promotion information from promotions.md and store in Promotion list

--- src/main/java/store/PromotionsFileReader.java
+++ src/main/java/store/PromotionsFileReader.java
@@ -1,4 +1,41 @@
 package store;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PromotionsFileReader {
+    final String productsFilePath = "src/main/resources/promotions.md";
+    List<Promotion> promotions = new ArrayList<>();
+
+    public PromotionsFileReader() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(productsFilePath))) {
+            readPromotions(reader);
+        } catch (IOException e) {
+            System.out.println("promotions.md 파일을 정상적으로 불러올 수 없습니다.");
+        }
+    }
+
+    private void readPromotions(BufferedReader reader) throws IOException {
+        String line = reader.readLine(); // header 제거
+        while ((line = reader.readLine()) != null) {
+            String[] elements = line.split(",");
+            setPromotions(elements);
+        }
+    }
+
+    private void setPromotions(String[] elements) {
+        String name = elements[0];
+        String buy = elements[1];
+        String get = elements[2];
+        String startDate = elements[3];
+        String endDate = elements[4];
+        promotions.add(new Promotion(name, buy, get, startDate, endDate));
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
+    }
 }
