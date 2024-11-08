9ac7e918239e9cad81911bc57faf27fa7203a0c2
mipangg <morganjeong119@gmail.com>
2024-11-08 11:53:59 +0900

    feat(Promotion): store promotion type, buy, get, start date and end date from promotions.md
    - check if the current date is on promotion

--- src/main/java/store/Promotion.java
+++ src/main/java/store/Promotion.java
@@ -1,4 +1,45 @@
 package store;
 
+import java.util.List;
+
 public class Promotion {
+    private String promotionType;
+    private int buy;
+    private int get;
+    private String startDate;
+    private String endDate;
+
+    public Promotion(String promotionType, String buy, String get, String startDate, String endDate) {
+        this.promotionType = promotionType;
+        this.buy = Integer.parseInt(buy);
+        this.get = Integer.parseInt(get);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getPromotionType() {
+        return promotionType;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public boolean isOnPromotion(String date) {
+        List<String> dates = List.of(startDate, date, endDate);
+        dates.sort(String::compareTo);
+        return dates.get(1).equals(date);
+    }
 }
