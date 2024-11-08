4307f603e741cd4b1fabd0800bf99c347df24373
mipangg <morganjeong119@gmail.com>
2024-11-08 11:54:42 +0900

    test(ProductsFileReaderTest): validate loading of products.md file

--- src/test/java/store/ProductsFileReaderTest.java
+++ src/test/java/store/ProductsFileReaderTest.java
@@ -1,4 +1,33 @@
 package store;
 
-public class ProductsFileReaderTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import store.Product;
+import store.ProductsFileReader;
+
+class ProductsFileReaderTest {
+    private ProductsFileReader fileReader;
+
+    @BeforeEach
+    void setUp() {
+        fileReader = new ProductsFileReader();
+    }
+
+    @Test
+    @DisplayName("상품 목록을 src/main/resources/products.md 파일에서 불러온다.")
+    void getProductsFromProductsMd() {
+        List<Product> products = fileReader.getProducts();
+
+        assertThat(products).isNotEmpty();
+        assertThat(products).hasSize(16);
+
+        assertThat(products.get(0).getName()).isEqualTo("콜라");
+        assertThat(products.get(0).getPrice()).isEqualTo(1000);
+        assertThat(products.get(0).getStock()).isEqualTo(10);
+        assertThat(products.get(0).getPromotion()).isEqualTo("탄산2+1");
+    }
 }
