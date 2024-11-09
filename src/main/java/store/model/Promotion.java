618b6be02327a89a85f7d326918beb09851a2844
mipangg <morganjeong119@gmail.com>
2024-11-10 04:25:28 +0900

    feat(Promotion): store promotion information and handle promotion discount
    - store promotion type, buy, get, start date, end date from promotions.md
    - calculate promotion discount price
    - check if user order enough quantity for promotion
    - check which promotions are available

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,4 +1,48 @@
 package store.model;
 
+import java.time.LocalDate;
+import java.util.List;
+import camp.nextstep.edu.missionutils.DateTimes;
+
 public class Promotion {
+    private String promotionType;
+    private int buy;
+    private int get;
+    private String startDate;
+    private String endDate;
+
+    public Promotion(String name, String buy, String get, String startDate, String endDate) {
+        this.promotionType = name;
+        this.buy = Integer.parseInt(buy);
+        this.get = Integer.parseInt(get);
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getPromotionType() {
+        return promotionType;
+    }
+
+    public int getPromotionDiscount(int quantity, int price) {
+        return (quantity / (buy + get)) * (((buy + get) * price) - (get * price));
+    }
+
+    public boolean isDivisible(int quantity) {
+        return quantity % (buy + get) == 0;
+    }
+
+    public boolean isAvailable() {
+        String today = getToday();
+        List<String> dates = List.of(startDate, today, endDate);
+        List<String> sortedDates = dates.stream()
+                .map(LocalDate::parse)
+                .sorted()
+                .map(LocalDate::toString)
+                .toList();
+        return sortedDates.get(1).equals(today);
+    }
+
+    private String getToday() {
+        return DateTimes.now().toString().split("T")[0];
+    }
 }
