9fdbd93d02003b8190e2e86272a43d3c83b37845
mipangg <morganjeong119@gmail.com>
2024-11-10 04:24:36 +0900

    feat(OrderItem): store user order items
    - store product and quantity
    - calculate product price

--- src/main/java/store/model/OrderItem.java
+++ src/main/java/store/model/OrderItem.java
@@ -1,4 +1,23 @@
 package store.model;
 
 public class OrderItem {
+    private Product product;
+    private int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTotalPrice() {
+        return product.getPrice() * quantity;
+    }
 }
