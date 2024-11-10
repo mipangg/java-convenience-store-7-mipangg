9209f51e769fef932a17c947ca8c342f1e9ee65d
mipangg <morganjeong119@gmail.com>
2024-11-11 03:39:39 +0900

    feat(Membership): update to get total price of a product by calling OrderItem's getTotalPrice()

--- src/main/java/store/model/Membership.java
+++ src/main/java/store/model/Membership.java
@@ -6,16 +6,10 @@ public class Membership {
     private final int MINIMUM_PRICE = 8000;
 
     public boolean isAvailableMembership(OrderItem orderItem) {
-        int totalPrice = getTotalPrice(orderItem);
-        return totalPrice >= MINIMUM_PRICE;
+        return orderItem.getTotalPrice() >= MINIMUM_PRICE;
     }
 
     public int getMembershipDiscount(OrderItem orderItem) {
-       int totalPrice = getTotalPrice(orderItem);
-       return (int) (totalPrice * 0.3);
-    }
-
-    private int getTotalPrice(OrderItem orderItem) {
-        return orderItem.getProduct().getTotalPrice(orderItem.getQuantity());
+       return (int) (orderItem.getTotalPrice() * 0.3);
     }
 }
