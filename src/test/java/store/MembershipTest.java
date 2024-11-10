c50db84dc07b1d21d4c9b4b5d00d05b6a2ec8c9b
mipangg <morganjeong119@gmail.com>
2024-11-10 15:08:04 +0900

    test(MembershipTest): test methods in Membership class

--- src/test/java/store/MembershipTest.java
+++ src/test/java/store/MembershipTest.java
@@ -1,4 +1,43 @@
 package store;
 
-public class MemberShipTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.fileReader.ProductsFileReader;
+import store.model.Membership;
+import store.model.OrderItem;
+import store.model.Product;
+
+public class MembershipTest {
+    private Membership membership;
+    private ProductsFileReader reader = new ProductsFileReader();
+    private OrderItem colaItem;
+    private OrderItem energyBarItem;
+
+    @BeforeEach
+    void setUp() {
+        membership = new Membership();
+        List<Product> products = reader.getProducts();
+        Product cola = products.get(0); // 콜라,1000,10,탄산2+1
+        Product energyBar = products.get(14); // 에너지바,2000,5,null
+
+        colaItem = new OrderItem(cola, 3);
+        energyBarItem = new OrderItem(energyBar, 5);
+    }
+
+    @Test
+    @DisplayName("멤버십 할인을 받기 위해서는 상품의 총 구매가격이 8,000원 이상이어야 한다.")
+    void beOverMinimumPriceForMembership() {
+        assertThat(membership.isAvailableMembership(colaItem)).isFalse();
+        assertThat(membership.isAvailableMembership(energyBarItem)).isTrue();
+    }
+
+    @Test
+    @DisplayName("회원은 프로모션 미적용 금액의 30%를 할인받는다.")
+    void get30PercentDiscountOfTotalPrice() {
+        assertThat(membership.getMembershipDiscount(energyBarItem)).isEqualTo(3000);
+    }
 }
