9906a6312d320046f9aebc23ebf1c5824b244d9f
mipangg <morganjeong119@gmail.com>
2024-11-10 15:07:41 +0900

    feat(Membership): add membership discount feature

--- src/main/java/store/model/Membership.java
+++ src/main/java/store/model/Membership.java
@@ -1,4 +1,21 @@
 package store.model;
 
-public class MemberShip {
+import store.model.OrderItem;
+
+public class Membership {
+    private final int MINIMUM_PRICE = 8000;
+
+    public boolean isAvailableMembership(OrderItem orderItem) {
+        int totalPrice = getTotalPrice(orderItem);
+        return totalPrice >= MINIMUM_PRICE;
+    }
+
+    public int getMembershipDiscount(OrderItem orderItem) {
+       int totalPrice = getTotalPrice(orderItem);
+       return (int) (totalPrice * 0.3);
+    }
+
+    private int getTotalPrice(OrderItem orderItem) {
+        return orderItem.getProduct().getTotalPrice(orderItem.getQuantity());
+    }
 }
