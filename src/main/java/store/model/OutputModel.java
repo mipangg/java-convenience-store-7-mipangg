a0f296935fd175234c8e2fce7777482610f93046
mipangg <morganjeong119@gmail.com>
2024-11-11 16:09:32 +0900

    feat(OutputModel): retrieve initial product list from Inventory and pass to OutputView

--- src/main/java/store/model/OutputModel.java
+++ src/main/java/store/model/OutputModel.java
@@ -1,4 +1,11 @@
 package store.model;
 
+import java.util.List;
+import store.model.Inventory;
+
 public class OutputModel {
+    public List<Product> getProducts() {
+        Inventory inventory = new Inventory();
+        return inventory.getTotalProducts();
+    }
 }
