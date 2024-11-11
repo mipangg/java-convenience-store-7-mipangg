3143f1cf132f7ec0bab062268d5449ba44a028be
mipangg <morganjeong119@gmail.com>
2024-11-11 16:08:34 +0900

    feat(StoreController): add methods to handle program execution sequence

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,11 +1,38 @@
-package store;
+package store.controller;
+
+import java.util.List;
+import store.model.InputModel;
+import store.model.Order;
+import store.model.Product;
+import store.view.InputView;
+import store.view.OutputView;
 
 public class StoreController {
+    InputView inputView;
+    OutputView outputView;
+    InputModel inputModel;
+    Order order;
 
     public StoreController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        inputModel = new InputModel();
+        order = new Order();
+    }
+
+    public void init() {
+        outputView.printProducts();
     }
 
     public void run() {
-
+        List<String> orderItems = inputModel.getUserOrders(inputView.readItem());
+        order.setOrderItems(orderItems);
+        order.calculateTotalAmountWithDiscount();
+        outputView.printReceipt(order.getReceipt());
+        if (inputModel.wantMoreShopping()) {
+            outputView.printUpdateProducts();
+            order.showAllProducts();
+            run();
+        }
     }
 }
