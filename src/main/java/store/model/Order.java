8e5968304e4f115576aa3ba2a37119650fbe8593
mipangg <morganjeong119@gmail.com>
2024-11-11 16:12:25 +0900

    feat(Order): handle user purchases
    - store list of user-ordered items
    - calculate final payment after checking promotion and membership discounts
    - generate receipt

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -1,4 +1,160 @@
 package store.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private final boolean CHECK = true;
+    private final boolean PASS = false;
+
+    private InputModel inputModel;
+    private Inventory inventory; // 일반 재고
+    private PromotionDiscount promotionDiscount; // 프로모션 재고
+    private AmountInfo amountInfo;// 금액 정보
+
+    private List<OrderItem> orderItems;
+    private Receipt receipt;
+    private boolean flag;
+
+    public Order() {
+        inputModel = new InputModel();
+        inventory = new Inventory();
+        promotionDiscount  = new PromotionDiscount();
+    }
+
+    public void setOrderItems(List<String> userOrders) {
+        List<OrderItem> userOrderItems = new ArrayList<>();
+        amountInfo = new AmountInfo();
+        for (String order : userOrders) {
+            String[] items = order.split("-");
+            Product product = inventory.getProduct(items[0]);
+            userOrderItems.add(new OrderItem(product, Integer.parseInt(items[1])));
+        }
+        orderItems = userOrderItems;
+        setReceipt();
+    }
+
+    public String getReceipt() { return receipt.getReceipt(); }
+
+    public List<OrderItem> getOrderItems() { return orderItems; }
+
+    public void calculateTotalAmountWithDiscount() {
+        applyPromotionDiscount();
+        applyMembershipDiscount();
+        calculateTotalAmount();
+        receipt.setAmountInfo(amountInfo);
+    }
+
+    private void applyPromotionDiscount() {
+        for (OrderItem orderItem : orderItems) {
+            getPromotionDiscount(orderItem);
+        }
+    }
+
+    private void applyMembershipDiscount() {
+        flag = inputModel.wantMembershipDiscount();
+        for (OrderItem orderItem : orderItems) {
+            amountInfo.addMembershipDiscount(getMembershipDiscount(orderItem));
+        }
+    }
+
+    private void calculateTotalAmount() {
+        for (OrderItem orderItem : orderItems) {
+            amountInfo.addAmount(orderItem.getTotalPrice());
+        }
+    }
+
+    // 멤버십 할인 금액 반환
+    private int getMembershipDiscount(OrderItem orderItem) {
+        Membership membership = new Membership();
+
+        if (! flag) { return 0; }
+        if (! membership.isAvailableMembership(orderItem)) { return 0; }
+
+        return membership.getMembershipDiscount(orderItem);
+    }
+
+    private int getPromotionDiscount(OrderItem orderItem) {
+        if (checkIsPromotion(orderItem) == CHECK) { return 0; }
+        if (checkOutOfStock(orderItem) == CHECK) { return 0; }
+        if (checkQuantityFormat(orderItem) == CHECK) { return 0;}
+        int discountAmount = promotionDiscount.calculatePromotionDiscount(orderItem);
+        amountInfo.addPromotionDiscount(discountAmount);
+        receipt.addGift(orderItem, promotionDiscount.getMatchedPromotion(orderItem));
+        return discountAmount;
+    }
+
+    private boolean checkIsPromotion(OrderItem orderItem) {
+        if (! promotionDiscount.isPromotion(orderItem)) {
+            inventory.reduceStock(orderItem.getProductName(), orderItem.getQuantity());
+            return CHECK;
+        }
+        return PASS;
+    }
+
+    // 프로모션 제품 재고가 부족한지 검사
+    private boolean checkOutOfStock(OrderItem orderItem) {
+        if (! orderItem.getProduct().isAvailable(orderItem.getQuantity())) {
+            askBuyRegulaProducts(orderItem);
+            return CHECK;
+        }
+        return PASS;
+    }
+
+    // 프로모션 행사 수량에 적합한지 검사
+    private boolean checkQuantityFormat(OrderItem orderItem) {
+        if (! promotionDiscount.isPromotionFormat(orderItem)) {
+            askAdditionalQuantity(orderItem);
+            return CHECK;
+        }
+        return PASS;
+    }
+
+    private void askBuyRegulaProducts(OrderItem orderItem) {
+        int quantity = orderItem.getQuantity();
+        int maximumQuantity = promotionDiscount.calculateMaximumQuantity(orderItem);
+
+        orderItem.setQuantity(maximumQuantity);
+        receipt.addGift(orderItem, promotionDiscount.getMatchedPromotion(orderItem));
+        amountInfo.addPromotionDiscount(getDiscountAmount(orderItem));
+       // orderItem.getProduct().reduceStock(maximumQuantity);
+        confirmRegularPurchase(orderItem, quantity, maximumQuantity);
+    }
+
+    private void confirmRegularPurchase(OrderItem orderItem, int quantity, int maximumQuantity) {
+        if(inputModel.wantToBuyRegularPrice(orderItem.getProductName(), quantity - maximumQuantity)) {
+            orderItem.setProduct(inventory.getProduct(orderItem.getProductName()));
+
+            int restQuantity = quantity - maximumQuantity - getRestPromotionStock(orderItem);
+            inventory.reduceStock(orderItem.getProductName(), restQuantity);
+            orderItem.setQuantity(quantity);
+        }
+    }
+
+    private int getRestPromotionStock(OrderItem orderItem) {
+        int restPromotionStock = promotionDiscount.getPromotionProductStock(orderItem);
+        promotionDiscount.reducePromotionStock(orderItem, restPromotionStock);
+        return restPromotionStock;
+    }
+
+    private int getDiscountAmount(OrderItem orderItem) {
+        return promotionDiscount.calculatePromotionDiscount(orderItem);
+    }
+
+    private void askAdditionalQuantity(OrderItem orderItem) {
+        if(inputModel.wantToAddMoreProduct(orderItem.getProductName())) {
+            orderItem.setQuantity(orderItem.getQuantity() + 1);
+        }
+    }
+
+    private void setReceipt() { receipt = new Receipt(orderItems); }
+
+    public void showAllProducts() {
+        List<Product> regularProducts = inventory.getRegularProducts();
+        List<Product> promotionProducts = promotionDiscount.getPromotionProducts();
+        regularProducts.stream()
+                .map(Product::toString).forEach(System.out::println);
+        promotionProducts.stream()
+                .map(Product::toString).forEach(System.out::println);
+    }
 }
