22de715855ea4578307ce34e22b741a283777002
mipangg <morganjeong119@gmail.com>
2024-11-11 16:10:02 +0900

    feat(OutputView): display product list and receipt to the user

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,4 +1,31 @@
 package store.view;
 
+import java.util.List;
+import store.model.OutputModel;
+import store.model.Product;
+
 public class OutputView {
+    OutputModel outputModel;
+
+    public OutputView() {
+        outputModel = new OutputModel();
+    }
+
+    public void printProducts() {
+        printTitle();
+        outputModel.getProducts().forEach(System.out::println);
+    }
+
+    public void printUpdateProducts() {
+        printTitle();
+    }
+
+    public void printReceipt(String receipt) {
+        System.out.println(receipt);
+    }
+
+    private void printTitle() {
+        System.out.println("\n안녕하세요. W편의점입니다.");
+        System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
 }
