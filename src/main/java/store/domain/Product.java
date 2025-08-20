59e5fe55a340b2b96a3df3b8ed40bf3cae082fee
mipangg <morganjeong119@gmail.com>
2025-08-20 16:36:11 +0900

    feat(Product): Product 초기 도메인 구현

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,5 +1,47 @@
 package store.domain;
 
+import store.exception.ErrorCode;
+
 public class Product {
+    private final String name;
+    private final int price;
+    private int stock;
+    private final Promotion promotion;
+
+    public Product(String name, int price, int stock, Promotion promotion) {
+        validate(name, price, stock);
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.promotion = promotion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void updateStock(int soldAmount) {
+        if (soldAmount > stock) {
+            throw new IllegalArgumentException(ErrorCode.EXCEED_STOCK.getMessage());
+        }
+        this.stock -= soldAmount;
+    }
+
+    public boolean isPromotion() {
+        if (promotion == null) {
+            return false;
+        }
+        return promotion.isActive();
+    }
+
+    private void validate(String name, int price, int stock) {
+        if (name == null || name.isEmpty() || price < 0 || stock < 0) {
+            throw new IllegalStateException(ErrorCode.INVALID_PRODUCT.getMessage());
+        }
+    }
 
 }
