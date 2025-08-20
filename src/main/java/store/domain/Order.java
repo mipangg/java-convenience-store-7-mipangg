aae9ab9851735539b0df587c7366dd7519a47078
mipangg <morganjeong119@gmail.com>
2025-08-20 16:34:42 +0900

    feat(Order): 주문 초기  도메인 구현

--- src/main/java/store/domain/Order.java
+++ src/main/java/store/domain/Order.java
@@ -11,4 +11,27 @@ public class Order {
     public Order(List<OrderItem> orderItems) {
         this.orderItems = orderItems;
     }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public int getTotalAmount() {
+        return totalAmount;
+    }
+
+    public int getPromotionDiscount() {
+        return promotionDiscount;
+    }
+
+    public int getMembershipDiscount() {
+        return membershipDiscount;
+    }
+
+    public void calculateTotalAmount() {
+        this.totalAmount = 0;
+        for (OrderItem orderItem : orderItems) {
+            this.totalAmount += orderItem.getTotalAmount();
+        }
+    }
 }
