55b838c74709305112ead76b393a7357b0a6bb22
mipangg <morganjeong119@gmail.com>
2025-08-20 16:35:24 +0900

    feat(OrderItem): OrderItem 초기 도메인 구현

--- src/main/java/store/domain/OrderItem.java
+++ src/main/java/store/domain/OrderItem.java
@@ -1,6 +1,25 @@
 package store.domain;
 
+import store.exception.ErrorCode;
+
 public class OrderItem {
-    private Product product;
-    private int quantity;
+    private final Product product;
+    private final int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        validate(product, quantity);
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public int getTotalAmount() {
+        return product.getPrice() * quantity;
+    }
+
+    private void validate(Product product, int quantity) {
+        if (product == null || quantity < 0) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_FORMAT.getMessage());
+        }
+    }
+
 }
