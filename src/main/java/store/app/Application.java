12ad0ca6fd86ea2a9065cc42af36ee45af2f3879
mipangg <morganjeong119@gmail.com>
2025-08-28 18:57:54 +0900

    feat(Store): prmotion 정보를 저장하는 기능 구현 및 흐름에 따라 각 메서드를 호출할 Store 클래스 생성

--- src/main/java/store/app/Application.java
+++ src/main/java/store/app/Application.java
@@ -3,5 +3,7 @@ package store.app;
 public class Application {
 
     public static void main(String[] args) {
+        Store store = new Store();
+        store.run();
     }
 }
--- src/main/java/store/app/Store.java
+++ src/main/java/store/app/Store.java
@@ -1,5 +1,53 @@
 package store.app;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import store.mapper.StoreMapper;
+import store.service.ProductManager;
+import store.service.PromotionManager;
+import store.util.StoreFileReader;
+import store.util.StoreParser;
+import store.view.StoreView;
+
 public class Store {
+
+    private final StoreFileReader fileReader;
+    private final StoreParser storeParser;
+    private final StoreMapper storeMapper;
+
+    private final StoreView storeView;
+
+    private final ProductManager productManager;
+    private final PromotionManager promotionManager;
+
+    public Store() {
+        fileReader = new StoreFileReader();
+        storeParser = new StoreParser();
+        storeMapper = new StoreMapper();
+        storeView = new StoreView();
+        productManager = new ProductManager();
+        promotionManager = new PromotionManager();
+    }
+
+    public void run() {
+
+    }
+
+    private void setPromotions() throws FileNotFoundException {
+        List<String> promotionInfos = fileReader.readPromotions();
+        List<Map<String, String>> parsedPromotionInfos = storeParser.getParsedInfos(promotionInfos);
+        for (Map<String, String> parsedPromotionInfo : parsedPromotionInfos) {
+            promotionManager.save(storeMapper.toPromotion(parsedPromotionInfo));
+        }
+    }
+
+    private void setProducts() throws FileNotFoundException {
+        List<String> productInfos = fileReader.readProducts();
+        List<Map<String, String>> parsedProductInfos = storeParser.getParsedInfos(productInfos);
+        for (Map<String, String> productInfo : parsedProductInfos) {
+
+        }
+    }
 
 }
