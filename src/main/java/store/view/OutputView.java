bfbe5406ea39861cee10ff2264266cd4626345a6
mipangg <morganjeong119@gmail.com>
2025-09-03 02:03:29 +0900

    feat(OutputView): 판매 상품 목록 출력 메서드의 인자 형식 변경

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,17 +1,19 @@
 package store.view;
 
 import java.util.List;
+import java.util.Map;
 import store.domain.Product;
 
 public class OutputView {
 
-    public void printProducts(List<Product> products) {
+    public void printProducts(Map<String, List<Product>> products) {
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.");
         System.out.println();
 
-        products.forEach(product -> System.out.println(product.toString()));
-        System.out.println();
+        products.forEach((k, v) -> {
+            v.forEach(System.out::println);
+        });
     }
 
     public void printErrorMessage(String errorMessage) {
